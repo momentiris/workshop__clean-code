bf1b84e3a40ff4b72ad99c74c624aaf654b83215
momentiris <momentiris@gmail.com>
2021-09-10 18:18:26 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import './style.css'
+
+const items = document.querySelectorAll<HTMLDivElement>('.cell')
+
+const wins = ['012', '345', '678', '036', '147', '258', '048', '246']
+
+const moves: { x: string[]; o: string[] } = {
+  x: [],
+  o: [],
+}
+
+const gameState = () => {
+  let p = false
+
+  return () => {
+    const setP = () => {
+      p = !p
+    }
+
+    return [p, setP] as [boolean, () => void]
+  }
+}
+
+const useGame = gameState()
+
+const handle = (event: Event) => {
+  const [isXPlayer, setPlayer] = useGame()
+  const cell = event.currentTarget as HTMLDivElement
+  const p = isXPlayer ? 'x' : 'o'
+
+  moves[p].push(cell.dataset.id as string)
+  cell.innerText = p
+
+  if (wins.some((win) => Array.from(win).every((x) => moves[p].includes(x)))) {
+    console.log(`${p} has won the game!`)
+    items.forEach((x) => x.removeEventListener('click', handle))
+  }
+
+  setPlayer()
+}
+
+items.forEach((cell) => cell.addEventListener('click', handle))
