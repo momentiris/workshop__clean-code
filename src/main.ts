e991a430c8e8abfb20a2e832e46d50429d214e43
momentiris <momentiris@gmail.com>
2021-09-22 17:54:11 +0200

    reset game

--- src/main.ts
+++ src/main.ts
@@ -3,42 +3,89 @@ import './style.css'
 const items = document.querySelectorAll<HTMLDivElement>('.cell')
 const grid = document.querySelector<HTMLDivElement>('.grid')
 
-const wins = ['012', '345', '678', '036', '147', '258', '048', '246']
+items.forEach((cell) => cell.addEventListener('click', handle))
+document.querySelector('#reset')?.addEventListener('click', () => {
+  const { r } = g()
+  r()
+  const box = document.querySelector('.win-modal--content')!
 
-const moves: { x: string[]; o: string[] } = {
-  x: [],
-  o: [],
+  box.classList.remove('active')
+  box.querySelector('span')!.textContent = ''
+  setTimeout(() => {
+    document.querySelector('.win-modal')?.classList.remove('active')
+    grid?.classList.remove('blur-sm')
+  })
+
+  items.forEach((cell) => {
+    cell.textContent = ''
+    cell.addEventListener('click', (event) => {
+      handle(event)
+    })
+  })
+})
+
+const win = (p: string) => {
+  document.querySelector('.win-modal')?.classList.add('active')
+  grid?.classList.add('blur-sm')
+  setTimeout(() => {
+    const box = document.querySelector('.win-modal--content')!
+
+    box.classList.add('active')
+    box.querySelector(
+      'span'
+    )!.textContent = `Player ${p.toUpperCase()} has won the game!`
+  })
 }
 
-const gameState = () => {
-  let p = false
+const wins = ['012', '345', '678', '036', '147', '258', '048', '246']
+
+const useGameState = () => {
+  let p = true
+
+  const moves: { x: string[]; o: string[] } = {
+    x: [],
+    o: [],
+  }
 
   return () => {
     const setP = () => {
       p = !p
     }
 
-    return [p, setP] as [boolean, () => void]
+    const setMoves = (p: 'x' | 'o', c: string) => moves[p].push(c)
+
+    const r = () => {
+      moves['x'] = []
+      moves['o'] = []
+      p = false
+    }
+
+    return { p, setP, moves, setMoves, r }
   }
 }
 
-const useGame = gameState()
+const g = useGameState()
 
-const handle = (event: Event) => {
-  const [isXPlayer, setPlayer] = useGame()
-  const cell = event.currentTarget as HTMLDivElement
-  const p = isXPlayer ? 'x' : 'o'
+function handle(event: Event) {
+  const { p: P, setP, moves, setMoves } = g()
 
-  moves[p].push(cell.dataset.id as string)
-  cell.innerText = p
+  const c = event.currentTarget as HTMLDivElement
+  const cId = c.dataset.id as string
+  const p = P ? 'x' : 'o'
+
+  if (
+    !Object.values(moves)
+      .flatMap((x) => x)
+      .some((x) => x === cId)
+  ) {
+    setMoves(p, cId)
+    c.innerText = p
+    setP()
+  }
 
   if (wins.some((win) => Array.from(win).every((x) => moves[p].includes(x)))) {
     console.log(`${p} has won the game!`)
     items.forEach((x) => x.removeEventListener('click', handle))
-    grid?.classList.add('blur-sm')
+    win(p)
   }
-
-  setPlayer()
 }
-
-items.forEach((cell) => cell.addEventListener('click', handle))
